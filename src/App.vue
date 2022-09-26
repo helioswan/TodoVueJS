ee7a77607a3047f87c50a2e5e49a64176bee6346
helioswan <60357194+helioswan@users.noreply.github.com>
2022-09-26 15:14:41 +0200

    Init project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Task from "./components/task.vue";
+import { ref } from "vue";
+
+const taskInput = ref("");
+const tasks = ref([]);
+
+const addTask = () => {
+  if (taskInput.value)
+    tasks.value.push({ title: taskInput.value, completed: false });
+  else alert("taskInput is empty");
+};
+const logTask = () => {
+  console.log(tasks.value);
+};
+</script>
+
+<template>
+  <main class="bg-gray-700">
+    <div class="container mx-auto min-h-screen">
+      <h1 class="text-xl text-center text-white">Todolist</h1>
+      <form>
+        <label>
+          Add task
+          <input type="text" v-model="taskInput" placeholder="Enter a task" />
+        </label>
+        <button @click.prevent="addTask">Submit</button>
+        <button @click.prevent="logTask">Log</button>
+      </form>
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index">
+          <Task :task="task" />
+        </li>
+      </ul>
+    </div>
+  </main>
+</template>
+<style></style>
---
+++ src/components/task.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>
+    {{ task.title }}
+  </p>
+  <form>
+    <label>
+      Completed
+      <input type="checkbox" v-model="task.completed" />
+    </label>
+  </form>
+</template>
+
+<script setup>
+defineProps({
+  task: Object,
+});
+</script>
